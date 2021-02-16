1eac4244b97e6ccc09193dacf00dbbf3a95f6241
Priyesh <priyeshsharma555@gmail.com>
2021-02-16 11:42:49 +0530

    Android Tab-layout Practise

---
+++ app/src/main/java/com/example/indicator/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.indicator;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.myToolbar);
+        tabLayout=(TabLayout)findViewById(R.id.tablayout);
+        viewPager=(ViewPager)findViewById(R.id.myViewPager);
+
+        setSupportActionBar(toolbar);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+    private void setupViewPager(View view){
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new StationFragment(),"Station");
+        viewPagerAdapter.addFragment(new AtoBFragment(),"A to B");
+        viewPagerAdapter.addFragment(new FairFragment(),"Fair");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+}
